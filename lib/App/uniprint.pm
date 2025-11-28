8d1684f392dff0e97f0e1c2657ad8ae2fc313e7e
perlancar <perlancar@gmail.com>
2025-11-28 09:21:38 +0700

    Initial commit

---
+++ lib/App/uniprint.pm
@@ -0,0 +1,17 @@
+package App::uniprint;
+
+use strict;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: Print each Unicode character of input, along with its codepoint and name
+
+=head1 SYNOPSIS
+
+See the command-line script L<uniprint>.
+
+=cut
